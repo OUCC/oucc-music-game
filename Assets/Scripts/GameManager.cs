fbd3b961eaa0e9a225a6de143792f1499c9f9f2f
yu195 <miyayuu.since2018@gmail.com>
2023-03-06 21:59:24 +0900

    モックを使って実装

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace OUCC.MusicGame
 {
     public class GameManager : MonoBehaviour
     {
+        public GameObject OriginalNote;
+
+        public LanePoint LanePoint;
+
+        private GameObject[] _notes;
+
         private void Start()
         {
 
@@ -13,12 +20,28 @@ namespace OUCC.MusicGame
 
         public void LoadNotes()
         {
-
+            var velocity = 1f;
+            var lanes = LanePoint.NoteLanePoint;
+            var notesInfo = new NoteMock[0];
+            _notes = notesInfo.Select(n =>
+            {
+                var noteObj = Instantiate(OriginalNote, CalculatePosition(velocity, n.timing, lanes[n.Lane]), Quaternion.identity);
+                var nc = noteObj.GetComponent<NoteContoroler>();
+                nc.NoteID = n.Id;
+                nc.NoteVelocity = velocity;
+                return noteObj;
+            }).ToArray();
         }
 
         public void StartPlay()
         {
 
+        }
+
+        public Vector3 CalculatePosition(float velocity, float timing, Vector3 lane)
+        {
+            lane.x += velocity * timing;
+            return lane;
         }
     }
 }
---
+++ Assets/Scripts/NoteMock.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OUCC.MusicGame
+{
+    internal class NoteMock
+    {
+        public int Id;
+        public int Lane;
+        public float timing;
+    }
+}
