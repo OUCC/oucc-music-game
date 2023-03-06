19c1d7b63954459f87c3d3abbe185ac7e09e31d5
frog3141 <fujisan_1415@yahoo.co.jp>
2023-03-06 11:18:39 +0900

    ノーツとレーンを制作

---
+++ Assets/Scripts/LanePoint.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OUCC.MusicGame
+{
+    public class LanePoint : MonoBehaviour
+    {
+        float NoteMakePoint = 95.0f;//ノーツを生成するZ座標、要調整
+        [SerializeField] List<GameObject> NoteLane = new List<GameObject>();
+        public List<Vector3> NoteLanePoint = new List<Vector3>();
+
+        [SerializeField] GameObject NoteCheker;
+        public float NoteChekPoint;//ノーツの判定を行うZ座標
+        // Start is called before the first frame update
+        void Awake()
+        {
+            //ノーツを生成する位置のリストの更新
+            for(int i = 0; i < NoteLane.Count; i++) 
+            {
+                NoteLanePoint.Add(new Vector3(NoteLane[i].transform.position.x, NoteLane[i].transform.position.y,NoteMakePoint));
+            }
+            //ノーツを判定する位置の更新
+            NoteChekPoint=NoteCheker.transform.position.z;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
---
+++ Assets/Scripts/NoteContoroler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OUCC.MusicGame
+{
+    public class NoteContoroler : MonoBehaviour
+    {
+        public float NoteVelocity;//ノーツの移動速度
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //ノーツの移動処理
+            this.gameObject.transform.position += Vector3.back * NoteVelocity;
+            //ノーツのz座標が一定以上になれば画面から消えたとみなしイベントを発行
+            if (this.gameObject.transform.position.z < -1f) Debug.Log("Miss!");
+        }
+        /// <summary>
+        /// ノーツを非表示にする関数
+        /// </summary>
+        public void NoteDestroy()
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+}
