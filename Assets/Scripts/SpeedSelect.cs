f35d34c515f62b7afda20df10f7adce06a8eb701
watanabe4869 <105962248+watanabe4869@users.noreply.github.com>
2023-04-13 09:42:27 +0900

    InputSystemを導入

--- Assets/Scripts/SpeedSelect.cs
+++ Assets/Scripts/SpeedSelect.cs
@@ -1,33 +1,40 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using TMPro;
 
 namespace OUCC.MusicGame
 {
     public class SpeedSelect : MonoBehaviour
     {
+        //速度表示
         [SerializeField] private TextMeshProUGUI SpeedText;
+        //速度
         private double Speed = 1.0;
-        // Start is called before the first frame update
+
+        //初期の速度表示
         void Start()
         {
             SpeedText.text = Speed.ToString("F");
         }
 
-        // Update is called once per frame
-        void Update()
+        //rightArrowキーを押した時速度を0.1上げる
+        public void OnSpeedUp(InputAction.CallbackContext context)
         {
-            if (Input.GetKeyUp(KeyCode.RightArrow))
-            {
-                Speed += 0.1;
-                SpeedText.text = Speed.ToString("F");
-            }
+            // 離された瞬間でPerformedとなる
+            if (!context.performed) return;
 
-            if (Input.GetKeyUp(KeyCode.LeftArrow))
-            {
-                Speed -= 0.1;
-                SpeedText.text = Speed.ToString("F");
-            }
+            Speed += 0.1;
+            SpeedText.text = Speed.ToString("F");
 
+        }
+        //rightArrowキーを押した時速度を0.1上げる
+        public void OnSpeeDown(InputAction.CallbackContext context)
+        {
+            // 離された瞬間でPerformedとなる
+            if (!context.performed) return;
+
+            Speed -= 0.1;
+            SpeedText.text = Speed.ToString("F");
 
         }
     }
