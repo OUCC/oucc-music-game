3e3eddc3275a9a0a7eaab6f36862f93016806a24
watanabe4869 <105962248+watanabe4869@users.noreply.github.com>
2023-04-23 12:54:02 +0900

    C#イベントに変更

--- Assets/Scripts/SpeedSelect.cs
+++ Assets/Scripts/SpeedSelect.cs
@@ -4,12 +4,39 @@ using TMPro;
 
 namespace OUCC.MusicGame
 {
+    [RequireComponent(typeof(PlayerInput))]
     public class SpeedSelect : MonoBehaviour
     {
         //速度表示
         [SerializeField] private TextMeshProUGUI SpeedText;
         //速度
         private double Speed = 1.0;
+
+        private PlayerInput _playerInput;
+
+        private void Awake()
+        {
+            //PlayerInputのインスタンスを取得
+            _playerInput = GetComponent<PlayerInput>();
+        }
+        private void OnEnable()
+        {
+            if (_playerInput == null) return;
+
+            // デリゲート登録
+            _playerInput.onActionTriggered += OnSpeedUp;
+            _playerInput.onActionTriggered += OnSpeedDown;
+        }
+
+        private void OnDisable()
+        {
+            if (_playerInput == null) return;
+
+            // デリゲート登録解除
+            _playerInput.onActionTriggered -= OnSpeedUp;
+            _playerInput.onActionTriggered -= OnSpeedDown;
+        }
+
 
         //初期の速度表示
         void Start()
@@ -20,21 +47,31 @@ namespace OUCC.MusicGame
         //rightArrowキーを押した時速度を0.1上げる
         public void OnSpeedUp(InputAction.CallbackContext context)
         {
-            // 離された瞬間でPerformedとなる
-            if (!context.performed) return;
+
+            // SpeedUp以外は処理しない
+            if (context.action.name != "SpeedUp") return;
+
+            //押された瞬間に動作
+            if (!context.started) return;
 
             Speed += 0.1;
             SpeedText.text = Speed.ToString("F");
 
         }
         //rightArrowキーを押した時速度を0.1上げる
-        public void OnSpeeDown(InputAction.CallbackContext context)
+        public void OnSpeedDown(InputAction.CallbackContext context)
         {
-            // 離された瞬間でPerformedとなる
-            if (!context.performed) return;
+            if (Speed >= 0.1)
+            {
+                // SpeedDown以外は処理しない
+                if (context.action.name != "SpeedDown") return;
 
-            Speed -= 0.1;
-            SpeedText.text = Speed.ToString("F");
+                //押された瞬間に動作
+                if (!context.started) return;
+
+                Speed -= 0.1;
+                SpeedText.text = Speed.ToString("F");
+            }
 
         }
     }
