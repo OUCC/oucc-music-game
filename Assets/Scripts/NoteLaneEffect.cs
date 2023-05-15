e948a18f7c67d6ca352448f2e1dd5fa9bf9e200c
frog3141 <fujisan_1415@yahoo.co.jp>
2023-05-15 17:50:26 +0900

    クラスとマテリアルを準備

---
+++ Assets/Scripts/NoteLaneEffect.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+
+namespace OUCC.MusicGame
+{
+    public class NoteLaneEffect : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        MeshRenderer laneMeshRenderer;
+        void Start()
+        {
+            laneMeshRenderer = GetComponent<MeshRenderer>();
+            laneMeshRenderer.material.EnableKeyword("_EMISSION");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            var keyboard = Keyboard.current;
+            if (keyboard != null)
+            {
+                //　スペースキーの状態の取得
+                if (keyboard.spaceKey.wasPressedThisFrame)LaneFlash();
+                if (keyboard.spaceKey.wasReleasedThisFrame) Debug.Log("SpaceKey:Up");
+            }
+        }
+        public void LaneFlash()
+        {
+            laneMeshRenderer.material.SetColor("_EmissionColor",new Color(0,1,0));
+        }
+    }
+}
